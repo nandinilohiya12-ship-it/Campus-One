26c7658b7f78d808eb94c631dcb9ff1baf8366a2
nandinilohiya12-ship-it <nandinilohiya12@gmail.com>
2026-08-23 21:39:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.CAMPUS_ONE_CONFIG = {
     supabaseUrl: "https://yoycosxaccatamhlcsvk.supabase.co",
     supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlveWNvc3hhY2NhdGFtaGxjc3ZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc0MDEyNjYsImV4cCI6MjEwMjk3NzI2Nn0.5QsWTmX7UQQI4AhLXR3sD9Iu7C-574yLMy08iwgGfog",
-    razorpayKeyId: "",
+    razorpayKeyId: "rzp_test_TTGsjmVWxqLE0V",
     demoMode: false
 };
